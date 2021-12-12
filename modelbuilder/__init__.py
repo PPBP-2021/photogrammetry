2577840c5bbb56077d11d71b04e50e7bd35f011d
Manuel <manuel.trageser789@googlemail.com>
2021-12-12 17:27:55 +0100

    first version of building a 3D litograph out of an 2D image, is buggy

--- modelbuilder/__init__.py
+++ modelbuilder/__init__.py
@@ -1,13 +1,49 @@
 import stl
 import numpy as np
+from stl.mesh import Mesh
+import cv2
+import image_utils
 
 
-def model_from_matrix(matrix: np.ndarray):
-    """[summary]
+from imageprocessing.segmentation import segmentate_grayscale
+
+
+def litograph_from_image(seg_img: np.ndarray, resolution: float):
+    """Construct a 3D litograph of a segmentated image.
 
     Parameters
     ----------
-    matrix : np.ndarray
-        [description]
+    seg_img : np.ndarray
+        Image segmentated into foreground, background. Background is completly black.
+    
+    resolution : float
+        Value between 0 and 1; the percentage of pixels used.
     """
-    ...
+    seg_img = cv2.resize(seg_img, (0,0),fx=resolution,fy=resolution)
+    seg_img = cv2.cvtColor(seg_img,cv2.COLOR_BGR2GRAY)
+    height, width = seg_img.shape
+  
+    vertices = []
+    faces = []
+
+    for i,row in enumerate(seg_img):
+        for j,pixel in enumerate(row):
+            Z = pixel / 255
+            vertices.append([j,height-i,Z])
+    
+    vertices = np.array(vertices)
+    vertices = vertices.reshape((height,width,3))
+    
+    for i in range(height-1):
+        for j in range(width-1):
+            top_left = vertices[i][j]
+            bottom_right = vertices[i+1][j+1]
+            face_1 = [top_left, vertices[i+1][j], bottom_right]
+            face_2 = [top_left, vertices[i][j+1], bottom_right]
+            faces.append(face_1)
+            faces.append(face_2)
+    
+    faces = np.array(faces,dtype=Mesh.dtype)
+    print(faces.shape)
+
+    return Mesh(faces)
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import imageprocessing as imgp
 import image_utils as imgutils
+import modelbuilder as mb
 
 
 seg_img = imgp.segmentate_grayscale(
-    r"testimages/man.jpg", 240, explain=True)
-imgutils.show_img(seg_img)
+    r"testimages/man.jpg", 240, explain=False)
+#imgutils.show_img(seg_img)
+litograph = mb.litograph_from_image(seg_img, 0.1)
+litograph.save("litograph.stl")
