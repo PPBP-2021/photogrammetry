bda1ebbd29fb1f1e766c0baf2864e625297000ac
Justin Sostmann <justin.sostmann@googlemail.com>
2021-12-12 18:01:16 +0100

    fixed lithophane function

--- modelbuilder/__init__.py
+++ modelbuilder/__init__.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import stl
 import numpy as np
 from stl.mesh import Mesh
@@ -8,32 +10,39 @@ import image_utils
 from imageprocessing.segmentation import segmentate_grayscale
 
 
-def litograph_from_image(seg_img: np.ndarray, resolution: float):
+def litograph_from_image(
+    seg_img: np.ndarray,
+    resolution: float = 1.0,
+    z_scale: Callable[[float], float] = lambda z: z
+) -> Mesh:
     """Construct a 3D litograph of a segmentated image.
 
     Parameters
     ----------
     seg_img : np.ndarray
         Image segmentated into foreground, background. Background is completly black.
-    
+
     resolution : float
         Value between 0 and 1; the percentage of pixels used.
+
+    z_scale : Callable[[float], float] to apply on the depth value. Original Z between 0 and 1.
     """
-    seg_img = cv2.resize(seg_img, (0,0),fx=resolution,fy=resolution)
-    seg_img = cv2.cvtColor(seg_img,cv2.COLOR_BGR2GRAY)
+
+    seg_img = cv2.resize(seg_img, (0, 0), fx=resolution, fy=resolution)
+    seg_img = cv2.cvtColor(seg_img, cv2.COLOR_BGR2GRAY)
     height, width = seg_img.shape
-  
+
     vertices = []
     faces = []
 
-    for i,row in enumerate(seg_img):
-        for j,pixel in enumerate(row):
+    for i, row in enumerate(seg_img):
+        for j, pixel in enumerate(row):
             Z = pixel / 255
-            vertices.append([j,height-i,Z])
-    
+            vertices.append([j/height, (height-i)/height, z_scale(Z)])
+
     vertices = np.array(vertices)
-    vertices = vertices.reshape((height,width,3))
-    
+    vertices = vertices.reshape((height, width, 3))
+
     for i in range(height-1):
         for j in range(width-1):
             top_left = vertices[i][j]
@@ -42,8 +51,8 @@ def litograph_from_image(seg_img: np.ndarray, resolution: float):
             face_2 = [top_left, vertices[i][j+1], bottom_right]
             faces.append(face_1)
             faces.append(face_2)
-    
-    faces = np.array(faces,dtype=Mesh.dtype)
-    print(faces.shape)
 
-    return Mesh(faces)
+    faces = np.array(faces)
+    mesh = Mesh(np.zeros(faces.shape[0], dtype=Mesh.dtype))
+    mesh.vectors = faces
+    return mesh
--- test.py
+++ test.py
@@ -2,9 +2,13 @@ import imageprocessing as imgp
 import image_utils as imgutils
 import modelbuilder as mb
 
+import math
+
 
 seg_img = imgp.segmentate_grayscale(
     r"testimages/man.jpg", 240, explain=False)
-#imgutils.show_img(seg_img)
-litograph = mb.litograph_from_image(seg_img, 0.1)
+# imgutils.show_img(seg_img)
+litograph = mb.litograph_from_image(seg_img,
+                                    resolution=0.5,
+                                    z_scale=lambda z: z/20)
 litograph.save("litograph.stl")
