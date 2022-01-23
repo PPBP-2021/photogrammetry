881afa6cdcb042e9770b3d3f4e1b88a89b27bc82
Justin Sostmann <justin.sostmann@googlemail.com>
2022-01-23 17:27:24 +0100

    added abbreviations to all slider titles

--- dashboard/layout/stereo_properties.py
+++ dashboard/layout/stereo_properties.py
@@ -1,14 +1,6 @@
-import glob
-import functools
-import os
-
-import dash
-from dash import html
-from dash import dcc
-
-
 import dash_bootstrap_components as dbc
-from matplotlib.pyplot import text
+from dash import dcc, html
+
 PROPERTY_STYLE = {
     "position": "fixed",
     "top": 0,
@@ -22,27 +14,75 @@ PROPERTY_STYLE = {
 
 layout = html.Div(
     [
-
         html.H2("Properties", className="display-10"),
         html.Hr(),
         dbc.Container(
             [
-            html.P("minDisparities", className="lead"),
-            dcc.Slider(id="minDisparity",min=0,step=1,max=500,value=0,tooltip={"placement": "bottom", "always_visible": True}),
-            html.P("numDisparities", className="lead"),
-            dcc.Slider(id="numDisparities",min=1,step=16,max=10*16,value=5*16,tooltip={"placement": "bottom", "always_visible": True}),
-            html.P("window_size", className="lead"),
-            dcc.Slider(id="window_size",min=1,step=2,max=31,value=5,tooltip={"placement": "bottom", "always_visible": True}),
-            html.P("disp12MaxDiff", className="lead"),
-            dcc.Slider(id="disp12MaxDiff",min=-1,step=1,max=100,value=12,tooltip={"placement": "bottom", "always_visible": True}),
-            html.P("uniquenessRatio", className="lead"),
-            dcc.Slider(id="uniquenessRatio",min=1,step=1,max=100,value=10,tooltip={"placement": "bottom", "always_visible": True}),
-            html.P("speckleWindowSize", className="lead"),
-            dcc.Slider(id="speckleWindowSize",min=0,step=50,max=200,value=50,tooltip={"placement": "bottom", "always_visible": True}),
-            html.P("speckleRange", className="lead"),
-            dcc.Slider(id="speckleRange",min=-1,step=1,max=5,value=5,tooltip={"placement": "bottom", "always_visible": True}),
-            html.P("preFilterCap", className="lead"),
-            dcc.Slider(id="preFilterCap",min=0,step=1,max=126,value=63,tooltip={"placement": "bottom", "always_visible": True}),
+                html.P(
+                    html.Abbr(
+                        "minDisparities", title="Minimum possible disparity value. Normally, it is zero but sometimes rectification algorithms can shift images, so this parameter needs to be adjusted accordingly."
+                    ), className="lead"
+                ),
+                dcc.Slider(id="minDisparity", min=0, step=1, max=500, value=0, tooltip={
+                           "placement": "bottom", "always_visible": True}),
+
+                html.P(
+                    html.Abbr(
+                        "numDisparities", title="Maximum disparity minus minimum disparity. The value is always greater than zero. In the current implementation, this parameter must be divisible by 16."
+                    ), className="lead"
+                ),
+                dcc.Slider(id="numDisparities", min=16, step=16, max=10*16, value=5*16,
+                           tooltip={"placement": "bottom", "always_visible": True}),
+
+                html.P(
+                    html.Abbr(
+                        "window_size", title="Matched block size. It must be an odd number >=1 . Normally, it should be somewhere in the 3..11 range."
+                    ),
+                    className="lead"
+                ),
+                dcc.Slider(id="window_size", min=1, step=2, max=31, value=5, tooltip={
+                           "placement": "bottom", "always_visible": True}),
+
+                html.P(
+                    html.Abbr(
+                        "disp12MaxDiff", title="Maximum allowed difference (in integer pixel units) in the left-right disparity check. Set it to a non-positive value to disable the check."
+                    ), className="lead"
+                ),
+                dcc.Slider(id="disp12MaxDiff", min=-1, step=1, max=100, value=12,
+                           tooltip={"placement": "bottom", "always_visible": True}),
+
+                html.P(
+                    html.Abbr(
+                        "uniquenessRatio", title="Margin in percentage by which the best (minimum) computed cost function value should 'win' the second best value to consider the found match correct. Normally, a value within the 5-15 range is good enough."
+                    ), className="lead"
+                ),
+                dcc.Slider(id="uniquenessRatio", min=1, step=1, max=100, value=10, tooltip={
+                           "placement": "bottom", "always_visible": True}),
+
+                html.P(
+                    html.Abbr(
+                        "speckleWindowSize", title="Maximum size of smooth disparity regions to consider their noise speckles and invalidate. Set it to 0 to disable speckle filtering. Otherwise, set it somewhere in the 50-200 range."
+                    ), className="lead"
+                ),
+                dcc.Slider(id="speckleWindowSize", min=0, step=50, max=200, value=50, tooltip={
+                           "placement": "bottom", "always_visible": True}),
+
+                html.P(
+                    html.Abbr(
+                        "speckleRange", title="Maximum disparity variation within each connected component. If you do speckle filtering, set the parameter to a positive value, it will be implicitly multiplied by 16. Normally, 1 or 2 is good enough."
+                    ), className="lead"
+                ),
+                dcc.Slider(id="speckleRange", min=-1, step=1, max=5, value=5,
+                           tooltip={"placement": "bottom", "always_visible": True}),
+
+                html.P(
+                    html.Abbr(
+                        "preFilterCap", title="Truncation value for the prefiltered image pixels. The algorithm first computes x-derivative at each pixel and clips its value by [-preFilterCap, preFilterCap] interval. The result values are passed to the Birchfield-Tomasi pixel cost function."
+                    ), className="lead"
+                ),
+                dcc.Slider(id="preFilterCap", min=0, step=1, max=126, value=63, tooltip={
+                           "placement": "bottom", "always_visible": True}),
+
             ]
         ),
 
--- dashboard/litophane_from_stereo.py
+++ dashboard/litophane_from_stereo.py
@@ -21,7 +21,7 @@ IMG_LEFT: Optional[np.ndarray] = None  # left image of the stereo pair
 IMG_RIGHT: Optional[np.ndarray] = None  # right image of the stereo pai
 
 # list of all PROPERTIES values used to call the Disparity calc function
-PROPERTY_VALS: List[int] = [0, 5*16, 5, 12, 10, 50, 32, 63]
+PROPERTY_VALS: List[int] = [0, 5*16, 5, 12, 10, 50, 5, 63]
 
 
 def calculate_current_disparity():
