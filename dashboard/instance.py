de125f9bd59f663b343b0d570a860d3f1913c8cd
Justin Sostmann <justin.sostmann@googlemail.com>
2022-01-28 14:59:05 +0100

    supresses callback exceptions, to ignore initial id not found errors

--- dashboard/instance.py
+++ dashboard/instance.py
@@ -7,6 +7,7 @@ external_stylesheets = [dbc.themes.COSMO, ]
 app = dash.Dash(
     __name__,
     external_stylesheets=external_stylesheets,
+    suppress_callback_exceptions=True
 )
 
 app_title = "Photogrammetry Practical"
