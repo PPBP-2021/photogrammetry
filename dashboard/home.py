39e2521294ede0d2de780cf8a4652c6ccb63c287
Manuel <manuel.trageser789@googlemail.com>
2022-09-13 16:33:25 +0200

    started working on practical description of the website

--- dashboard/home.py
+++ dashboard/home.py
@@ -61,6 +61,88 @@ So we decided to learn more about it and create our own photogrammetry project.
 
 ![name](assets/home/logo.png)
 
+--------------------------
+
+## Practice
+
+#### Used modules
+- Dash
+    - website
+- OpenCV
+    - general image library
+- Open3D
+    - meshes and point clouds
+- Plotly
+    - plot resuslts
+
+-----------------------
+To get used to working with the different Python libraries and working with images, point clouds and meshes in general we first approached two tasks, which are not directly related to the goal of our practical.
+These two are Image Background segmentation and creating Litophanes of images.
+Afterwards we started working on our Stereo Photogrammetry approach.
+
+
+### Segmentation
+
+#### Description
+Seperate the main object of an image from the image background
+#### Algorithm
+1. Shadow Reduction by converting the image to HSV format and turning up brightness, then back to BGR
+2. Convertion to grayscale
+3. Use of a fixed threshold to create a mask (make all grayscal values higher than x completly black and all others white)
+4. Use openCV to smooth the edges of the mask and to remove noise
+5. Find the largest contour in the mask (since this is the one most probable to cover the wanted object) --> remove all other contours
+6. apply mask to image
+#### Results
+
+![name](assets/home/segmentation.png) ![name](assets/home/segmentation2.png)
+
+### Litophane
+#### Description
+
+Litophane is an old technique. It uses a plate with engraved contours. When light passes through the plate parts with thicker contours are darker (since less light can pass).
+Our application turns this approach around. We assume that darker spots of an image are located in the front of the 3D scene while brighter spots are in the back.
+
+#### Algorithm
+
+Using the color values directly as depth informatio
+
+#### Results
+
+![name](assets/home/litophane2.png) ![name](assets/home/litophane.png)
+
+### Stereo Photogrammetry
+
+#### Prerequisite
+Having 2 Stereo Image pairs, showing the same scene which are horizontally shifted by a certain baseline.
+#### 1. Rectification
+To make it easier for the disparity map algorithm we performed a rectification on our images pairs to correct non perfect horizontal shifts.
+When the image pairs are perfectly shifted and therefore lay on the same plane the epipolar lines, as explained in the Theory, are perfectly horizontal and therefore finding the same pixels on both images to calculate the disparity between them is reduced to a search on the epipolar line.
+To do so we used a SIFT keypoint matching algorithm by OpenCV, can find some very unique points on both images and match them (it is important to note that this keypoint matching can not be used later for the disparity map since it requires all pixles to be matched and the SIFT keypoint matcher can only find certain ones, so it is sensible to rectify images so the later pixel matching is easier).
+We then calculated the epipolar lines between the found keypoints. Now the images need to be transformed until these are perfetly horizontal. Then the image is rectified
+##### Results
+
+
+#### 2. Disparity Map
+We used the openCV StereoBSGM algorithm to perform a block/pixel matching between our rectified image pairs and to calculate the displacement vector between those blocks/pixels --> disparity map.
+##### Problems when calculating disparity map
+- need of patterns on objects
+- baseline shift between image pairs needs to be chosen so that an parallax effect can be seen
+- requires good depth of field
+##### Results
+
+#### 3. Stereo Point Cloud
+The depth of every pixel in the image can be recovered  of the disparity map by INSERT FORMULA as explained in the theory.
+
+##### Results
+
+#### 3D Model / Mesh
+To create a full 3D Mesh out of our Point clouds we need to calculate such a point cloud for every side of an object (object from the left, right, top etc.)
+Then these different Models from different perspectives need to be matched togheter, which we did not further study in our project, but which could probably again be done by some sort of keypoint matching.
+
+##### Results
+
+
+
 """,
         highlight_config={"theme": "dark"},
         style={"margin": "0 auto", "width": "50%", "textAlign": "start"},
