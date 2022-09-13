93f93234ccce5007d2ff8f7caf53129f5cb139b5
Justin Sostmann <justin.sostmann@googlemail.com>
2022-09-13 15:12:09 +0200

    Rewrite home page.

--- dashboard/home.py
+++ dashboard/home.py
@@ -8,45 +8,58 @@ layout = [
     dcc.Markdown(
         """
 # Photogrammetry Beginners Practical WS 2021/22
------------------------------------------------
 ```py
 print("Welcome to our Project Page")
 ```
 `>>>"Welcome to our Project Page"`
 
+-----------------------------------------------
+
 ## What is Photogrammetry?
+Photogrammetry is the science of making measurements from photographs.
+It is used to reconstruct the third dimension (the depth) of two-dimensional images.
+The application of photogrammetry is widespread and includes surveying, measuring, modeling, and 3D reconstruction.
 
-Photogrammetry is the process of creating 3D objects from 2D photographs. The process can be used to scan
- anything from objects and people, to architecture, terrain and landscapes. There are various strategies
- you can use when taking photos to produce accurate models. This document includes photography
- techniques, ideal camera settings and lighting, photogrammetry software, and further resources to
- create 3D models from photos.
+### Related fields
+- land surveying
+    - google earth
+- computer vision
+    - autonomous driving
+- computer graphics
+    - 3D modeling
 
-### Definition
+### Approaches
+- Shape from Shading
+    - reconstruct the shape of an object using shadow and light information
+- Structure from Motion
+    - reconstruct the 3D structure of a scene from a set of 2D images
+- **Stereo-photogrammetry** (our approach)
+    - keypoint triangulation
 
-  **Photogrammetry** *[noun]* The science or technique for obtaining reliable information on the
-   natural environment or physical objects by recording, measuring and interpreting photographic images.
+-----------------
+## Motivation
+In the past we worked on a video game. One of the main challenges was to create good looking 3D models.
+Creating 3D models is a time consuming task.
+We already knew of *photogrammetry* as a way to automate model generation, but didn't know how it worked.
+Furthermore we are generally interested in *computer vision* and *image processing*.
+So we decided to learn more about it and create our own photogrammetry project.
 
-  Greek:
 
-  - *"photos"* (light)
-  - *"gramma"* (something written or drawn)
-  - *"metron"* (measure)
+### Goals
+- learn about camera basics
+    - 3D to 2D projection
+- learn about image processing
+    - implement in python
+ - generate meshes in python
+    - different mesh formats
 
-#### Typical Outputs
+**➤ generate 3D-model from images**
 
-A map, a drawing, a 3D model of a real-world object, scene, or terrain.
+-------------------------
+## Theory
 
-#### Related fields
 
-Remote Sensing, GIS, Stereoscopy
-
-#### Main Tasks of Photogrammetry
-
-  - To measure something without touching it
-  - To measure something that may no longer exist, or may only exist in photographs
-  - To measure something too large to measure with traditional methods, i.e., landscape, a megalithic structure
-  - Quantitative data from photographs, the science of measuring in photos.
+![name](assets/home/logo.png)
 
 """,
         highlight_config={"theme": "dark"},
